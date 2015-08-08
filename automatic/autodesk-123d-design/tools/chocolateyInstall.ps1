b5829f6b819b176b7faa7402600031533b1cb717
Vaquero84 <haag.olaf@gmail.com>
2015-08-08 04:03:42 +0200

    Licence Acceptance required

--- automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
+++ automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
@@ -1,11 +1,3 @@
-﻿#Items that could be replaced based on what you call chocopkgup.exe with
-#{{PackageName}} - Package Name (should be same as nuspec file and folder) |/p
-#{{PackageVersion}} - The updated version | /v
-#{{DownloadUrl}} - The url for the native file | /u
-#{{PackageFilePath}} - Downloaded file if including it in package | /pp
-#{{PackageGuid}} - This will be used later | /pg
-#{{DownloadUrlx64}} - The 64bit url for the native file | /u64
-
 $packageName = '{{PackageName}}'
 $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
