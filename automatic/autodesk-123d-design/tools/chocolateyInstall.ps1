b20b8c297c3217b33e2e68699f8f2bd69b711c8e
Vaquero84 <haag.olaf@gmail.com>
2015-08-08 03:14:52 +0200

    initial setup

--- automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
+++ automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
@@ -1,1 +1,16 @@
+﻿#Items that could be replaced based on what you call chocopkgup.exe with
+#{{PackageName}} - Package Name (should be same as nuspec file and folder) |/p
+#{{PackageVersion}} - The updated version | /v
+#{{DownloadUrl}} - The url for the native file | /u
+#{{PackageFilePath}} - Downloaded file if including it in package | /pp
+#{{PackageGuid}} - This will be used later | /pg
+#{{DownloadUrlx64}} - The 64bit url for the native file | /u64
 
+$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '/SD'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
