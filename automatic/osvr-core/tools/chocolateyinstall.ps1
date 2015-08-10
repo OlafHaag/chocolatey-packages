06f82f4bdf57da9e1c4b6edc422371bda8bb8b3e
Vaquero84 <haag.olaf@gmail.com>
2015-08-10 06:46:31 +0200

    removed try/catch and Write-ChocolateySuccess, because the log told me to

--- automatic/osvr-core/tools/chocolateyinstall.ps1
+++ automatic/osvr-core/tools/chocolateyinstall.ps1
@@ -2,13 +2,6 @@ $packageName = '{{PackageName}}'
 $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 
-try {
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
