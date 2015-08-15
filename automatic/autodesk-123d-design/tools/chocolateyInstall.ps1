191a54d802e7fd39b1733784fddee5c42b044bb3
Vaquero84 <haag.olaf@gmail.com>
2015-08-15 02:55:42 +0200

    add exit code 1223 (cancelled by user) which, strange enough, is output by the SUCCESSFUL unattended installation

--- automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
+++ automatic/autodesk-123d-design/tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/S'
-$validExitCodes = @(0)
+$validExitCodes = @(0,1223)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
