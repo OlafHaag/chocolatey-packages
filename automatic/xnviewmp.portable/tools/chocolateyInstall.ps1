8e29ae0e12444a5db31a4f3b16b452eedf54dc29
Vaquero84 <haag.olaf@gmail.com>
2015-08-18 06:55:02 +0200

    xnviewmp tri-package (meta, install, portable)

---
+++ automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
