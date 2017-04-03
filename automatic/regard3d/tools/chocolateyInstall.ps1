82cde9472c35be6954ca7e8c04ab82865ca31a3c
Olaf Haag <haag.olaf@gmail.com>
2017-04-03 18:32:29 +0200

    regard3d package introduced

---
+++ automatic/regard3d/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url64 = '{{DownloadUrlx64}}'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url64bit $url64 `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum64 $checksum64 `
+                          -ChecksumType64 $checksumType64
