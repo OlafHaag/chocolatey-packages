c52b73e0d7cf12b96caa48d21e639bcc945579bb
Olaf Haag <haag.olaf@gmail.com>
2017-04-03 16:18:34 +0200

    fix for 32/64bit releases

--- automatic/drawpile/tools/chocolateyInstall.ps1
+++ automatic/drawpile/tools/chocolateyInstall.ps1
@@ -1,14 +1,20 @@
 $packageName = '{{PackageName}}'
-$installerType = 'EXE'
+$installerType = 'exe'
 $silentArgs = '/SILENT'
 $url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha256'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
 $validExitCodes = @(0)
 Install-ChocolateyPackage -PackageName $packageName `
                           -FileType $installerType `
                           -SilentArgs $silentArgs `
                           -Url $url `
+                          -Url64bit $url64 `
                           -ValidExitCodes $validExitCodes `
                           -Checksum $checksum `
-                          -ChecksumType $checksumType
+                          -ChecksumType $checksumType `
+                          -Checksum64 $checksum64 `
+                          -ChecksumType64 $checksumType64
