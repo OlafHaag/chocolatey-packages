3b0f978917aa2af90dc5ea57b207e2ee03630e42
Olaf Haag <haag.olaf@gmail.com>
2017-07-21 16:48:52 +0200

    meshmixer changes

--- automatic/autodesk-meshmixer/tools/chocolateyInstall.ps1
+++ automatic/autodesk-meshmixer/tools/chocolateyInstall.ps1
@@ -1,20 +1,14 @@
 $packageName = '{{PackageName}}'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha256'
 $checksum64 = '{{Checksumx64}}'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,1223)
 Install-ChocolateyPackage -PackageName $packageName `
                           -FileType $installerType `
                           -SilentArgs $silentArgs `
-                          -Url $url `
                           -Url64bit $url64 `
                           -ValidExitCodes $validExitCodes `
-                          -Checksum $checksum `
-                          -ChecksumType $checksumType `
                           -Checksum64 $checksum64 `
                           -ChecksumType64 $checksumType64
