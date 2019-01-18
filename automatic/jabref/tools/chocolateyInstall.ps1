d44b04df45bc52e2ed1d2cbd9b455768771591c3
Olaf Haag <haag.olaf@gmail.com>
2019-01-18 21:37:02 +0100

    jabref au conversion

--- automatic/jabref/tools/chocolateyInstall.ps1
+++ automatic/jabref/tools/chocolateyInstall.ps1
@@ -1,20 +1,15 @@
-$packageName = '{{PackageName}}'
-$installerType = 'exe'
-$silentArgs = '-q'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha256'
-$checksum64 = '{{Checksumx64}}'
-$checksumType64 = 'sha256'
-$validExitCodes = @(0)
-Install-ChocolateyPackage -PackageName $packageName `
-                          -FileType $installerType `
-                          -SilentArgs $silentArgs `
-                          -Url $url `
-                          -Url64bit $url64 `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum $checksum `
-                          -ChecksumType $checksumType `
-                          -Checksum64 $checksum64 `
-                          -ChecksumType64 $checksumType64
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'jabref'
+  fileType       = 'exe'
+  url            = 'https://github.com/JabRef/jabref/releases/download/v4.3.1/JabRef_windows_4_3_1.exe'
+  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v4.3.1/JabRef_windows-x64_4_3_1.exe'
+  checksum       = '7abe526bccdcea38df236a3879bb60f7d444d064dd51a3fc31bc0954e504b28e'
+  checksum64     = '5ca382423459dc15d382f5bf6d0acd2194e98bda46b8b14642784d4f8a51f8ff'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '-q'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/jabref/update.ps1
@@ -0,0 +1,43 @@
+import-module au
+import-module Wormies-AU-Helpers
+
+$releases = 'https://github.com/JabRef/jabref/releases'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "JabRef_windows-x64.*.exe"
+    $url64 = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url64 = 'https://github.com' + $url64
+
+    $url32 = $url64 -replace '-x64', ''
+
+    $version = $url64 -split 'x64_|.exe' | select -Last 1 -Skip 1
+    $version = $version -replace '_', '.'
+
+    return @{
+        URL32        = $url32
+        URL64        = $url64
+        Version      = Get-FixVersion $version
+        ReleaseNotes = "https://github.com/JabRef/jabref/blob/v${version}/CHANGELOG.md"
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')" = "`$1'$($Latest.FileType)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+update
