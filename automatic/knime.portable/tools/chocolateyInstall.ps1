6a578a992e86ed9e53257e4bb6bf6cb7e382dc04
Olaf Haag <haag.olaf@gmail.com>
2019-01-19 20:26:57 +0100

    packagesArgs url-key fix

--- automatic/knime.portable/tools/chocolateyInstall.ps1
+++ automatic/knime.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'knime.portable'
-  $url           = 'https://download.knime.org/analytics-platform/win/knime_3.6.2.win32.win32.x86.zip'
+  url           = 'https://download.knime.org/analytics-platform/win/knime_3.6.2.win32.win32.x86.zip'
   url64bit       = 'https://download.knime.org/analytics-platform/win/knime_3.6.2.win32.win32.x86_64.zip'
   checksum       = '73a0b91dff13d297bfd90e9afc157f9a727b81fae9a1eefb3f69c4625bd1220c'
   checksumType   = 'sha256'
