1eecd4f16f9d4ffa2c0cc3e000f663a7f36dd3e5
Olaf Haag <haag.olaf@gmail.com>
2019-03-10 01:57:32 +0100

    drawpile: no win32 available

--- automatic/drawpile/update.ps1
+++ automatic/drawpile/update.ps1
@@ -10,12 +10,12 @@ function global:au_GetLatest {
     $url64 = $download_page.links | ? href -match $re | select -First 1 -expand href
     $url64 = 'https://drawpile.net' + $url64
 
-    $url32 = $url64 -replace '.exe', '-w32.exe'
+    # $url32 = $url64 -replace '.exe', '-w32.exe'
 
     $version = $url64 -split '-|.exe' | select -Last 1 -Skip 2
 
     return @{
-        URL32        = $url32
+        # URL32        = $url32
         URL64        = $url64
         Version      = Get-FixVersion $version
         ReleaseNotes = "https://drawpile.net/news/release-${version}"
@@ -27,9 +27,9 @@ function global:au_SearchReplace {
         "tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
             "(?i)(^\s*fileType\s*=\s*)('.*')" = "`$1'$($Latest.FileType)'"
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            #"(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            # "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
 
