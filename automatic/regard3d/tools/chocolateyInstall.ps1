6c5d555af4aca671e288d7dcede1557d072986ca
Chocolatey <chocolatey@realdimensions.net>
2019-03-15 21:31:23 +0000

    AU: 1 updated - regard3d [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/4becc91e9e31a7f03ddf9685ad08e3891a98886a

--- automatic/regard3d/tools/chocolateyInstall.ps1
+++ automatic/regard3d/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'regard3d'
   fileType       = 'exe'
-  url64bit       = 'https://sourceforge.net/projects/regard3d/files/Regard3D/0.9.5/Regard3D_0.9.5_Setup.exe/download'
-  checksum64     = 'ca76a607220c0ce2ccd77d862407810ff1fbddb127e7d4b79f36901c87f33dc6'
+  url64bit       = 'https://sourceforge.net/projects/regard3d/files/Regard3D/1.0.0/Regard3D_1.0.0_Setup.exe/download'
+  checksum64     = '8e3cf81b3fd5baae606c3634465432df8633c1cb24c414c6f07920774352a57b'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
