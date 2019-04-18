d746d1152f8606abf393fd66aa1a9c8267e0cb2a
Chocolatey <chocolatey@realdimensions.net>
2019-04-18 21:32:49 +0000

    AU: 2 updated - knime knime.install [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/c9b83032abee1104e7cc8c17a71d6955c4e7eb6e

--- automatic/knime.install/tools/chocolateyInstall.ps1
+++ automatic/knime.install/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'knime.install'
   fileType       = 'exe'
-  url            = 'https://download.knime.org/analytics-platform/win/KNIME%203.7.1%20Installer%20%2832bit%29.exe'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%203.7.1%20Installer%20%2864bit%29.exe'
-  checksum       = '2883f83c39a38f27126253c435fa34513f3aad5c155d6928617579d4817f3b7f'
+  url            = 'https://download.knime.org/analytics-platform/win/KNIME%203.7.2%20Installer%20%2832bit%29.exe'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%203.7.2%20Installer%20%2864bit%29.exe'
+  checksum       = '6e0fba8b948150348925863c82e405e0a08136fa12cf8f1b894a764f131da428'
   checksumType   = 'sha256'
-  checksum64     = 'd28efee138a87a2baf2ca6507b6b0eb7c41a36dfdf4f03bbd8afb8b145bcb143'
+  checksum64     = '207c3e7843663a0c74b63750f96d9e88598c34b2516b352d14a2d4b7c691bf2a'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
