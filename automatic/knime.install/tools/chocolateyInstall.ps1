ce899da3913a28f20488ae06357024dc56483dee
Chocolatey <chocolatey@realdimensions.net>
2019-08-15 21:32:23 +0000

    AU: 3 updated - knime knime.install knime.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/120674a31d2cfa48aed5669b9d6152030b6e546d

--- automatic/knime.install/tools/chocolateyInstall.ps1
+++ automatic/knime.install/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'knime.install'
   fileType       = 'exe'
-  url            = 'https://download.knime.org/analytics-platform/win/KNIME%204.0.0%20Installer%20%2832bit%29.exe'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.0.0%20Installer%20%2864bit%29.exe'
-  checksum       = '6f6c4dace558edabf6bc94c0a3476daec3e6261868f6a5bd2d9f1749a53f44f1'
+  url            = 'https://download.knime.org/analytics-platform/win/KNIME%204.0.1%20Installer%20%2832bit%29.exe'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.0.1%20Installer%20%2864bit%29.exe'
+  checksum       = 'ec2b18d0ebb43fa632198f23bd8d3ff240acf2ad5650bbc41f7338a4fd188456'
   checksumType   = 'sha256'
-  checksum64     = '4f60246f91b3387d0f40d07ad0f864b9e2785a9672353aae2c38ac13b0019b28'
+  checksum64     = '49af1d1298c55aeb91a88098eef4c29478a7cbf83d511b2ba0b6cbec1d3ff4e9'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- automatic/knime.portable/tools/chocolateyInstall.ps1
+++ automatic/knime.portable/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   packageName    = 'knime.portable'
-  url            = 'https://download.knime.org/analytics-platform/win/knime_4.0.0.win32.win32.x86.zip'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.0.0.win32.win32.x86_64.zip'
-  checksum       = '9060477ab71c5ef7c5cdc79ab1ebe63159b07064ce3d85eebe97c58c06b3c41a'
+  url            = 'https://download.knime.org/analytics-platform/win/knime_4.0.1.win32.win32.x86.zip'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.0.1.win32.win32.x86_64.zip'
+  checksum       = '165b951990f7dd31b1bb51772187810428509cf7352d68d9767339659269a834'
   checksumType   = 'sha256'
-  checksum64     = '3147bfbb3accd99fb581a6382ed4b002c6e4303776cbd0d371c4cf0e60714a47'
+  checksum64     = '7d86fec6f7bd1d4a2519294fb9773909783307bc206f871dc53e66488a6e4bca'
   checksumType64 = 'sha256'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   validExitCodes = @(0)
