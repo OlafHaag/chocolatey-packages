7609f03268126266b2edde89f45684ffb36edb5b
Chocolatey <chocolatey@realdimensions.net>
2019-08-26 17:09:33 +0000

    AU: 1 updated - jabref [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/04eb365ec957c7961666f50b8c04b8c91cc5076b

--- automatic/jabref/tools/chocolateyInstall.ps1
+++ automatic/jabref/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'jabref'
   fileType       = 'exe'
-  url            = 'https://github.com/JabRef/jabref/releases/download/v4.3.1/JabRef_windows_4_3_1.exe'
-  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v4.3.1/JabRef_windows-x64_4_3_1.exe'
-  checksum       = '7abe526bccdcea38df236a3879bb60f7d444d064dd51a3fc31bc0954e504b28e'
-  checksum64     = '5ca382423459dc15d382f5bf6d0acd2194e98bda46b8b14642784d4f8a51f8ff'
+  url            = 'https://github.com/JabRef/jabref/releases/download/v5.0-alpha/JabRef_windows-x32_5_0-alpha.exe'
+  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.0-alpha/JabRef_windows-x64_5_0-alpha.exe'
+  checksum       = '8be70bf0f46b5f433ef959d80a65e24d03c94d4485e3ab4cda400a67cfe1c9c1'
+  checksum64     = 'dde2a8b76e8602b758c7dd1205a2d430187340fa61ee0cb3a952e82127dda565'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '-q'
