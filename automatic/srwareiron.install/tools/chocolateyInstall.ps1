a821a635be93eb2e81357a3753a00129728a4d4c
Chocolatey <chocolatey@realdimensions.net>
2019-10-15 21:34:45 +0000

    AU: 3 updated - srwareiron srwareiron.install srwareiron.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/42b9ce119eaba8669b270368753fc8b8af00702a

--- automatic/srwareiron.install/tools/chocolateyInstall.ps1
+++ automatic/srwareiron.install/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.srware.net/downloads/srware_iron.exe'
   url64bit       = 'https://www.srware.net/downloads/srware_iron64.exe'
-  checksum       = '66698322e2968c925b53de2b1a1e1f04b30f5ad743287666acdc201f5978a7f2'
+  checksum       = 'c0a9d7ae196093feb91bb400b70d3ed31c08afb2b6e5f9e262de0a372b2f9d9f'
   checksumType   = 'sha256'
-  checksum64     = 'd5027620e699ac6d9875d104bfc5c0e11844b9356ef135cf76e8e27ab76ddcc3'
+  checksum64     = 'e0fef56ce5da3f49b83fc7016fcc2ddc667cae44ad5f2fd7aab0b273aa23eaa3'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Components=Iron'
   validExitCodes = @(0)
