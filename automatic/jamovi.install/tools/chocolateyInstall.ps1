f8d4989311b15e16302b7d0ad4058bb1b3051635
Gootjes <37846483+Gootjes@users.noreply.github.com>
2019-11-08 17:20:40 +0100

    reverted 1.1.7-current to 1.0.7 in nuspec

--- automatic/jamovi.install/tools/chocolateyInstall.ps1
+++ automatic/jamovi.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'jamovi.install'
   fileType       = 'exe'
-  url64bit       = 'https://www.jamovi.org/downloads/jamovi-1.1.7.0-win64.exe'
-  checksum64     = '374b9da5b5c263b45b4ccd6bca6f93733f61e9aa316119910e71dee808ddcf49'
+  url64bit       = 'https://www.jamovi.org/downloads/jamovi-1.0.7.0-win64.exe'
+  checksum64     = 'cde72f12e268fc4df358629347a3e8e5f4714df3aebba071c5bbe41d7a3f68b8'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
