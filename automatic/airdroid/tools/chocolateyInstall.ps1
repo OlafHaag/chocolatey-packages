d2decd7ee83f793cd697a0254f27a2e9eba422c5
Chocolatey <chocolatey@realdimensions.net>
2019-11-14 21:31:44 +0000

    AU: 1 updated - airdroid [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/7a1ce29918fe1069773a2552e8800f61ad6dfb13

--- automatic/airdroid/tools/chocolateyInstall.ps1
+++ automatic/airdroid/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'airdroid'
   fileType       = 'exe'
-  url            = 'http://dl.airdroid.com/AirDroid_Desktop_Client_3.6.6.1.exe'
-  checksum       = '5a53de906a32270bd48ceaaa863fea4345904b5dcf02317d9b1b89cf0513f785'
+  url            = 'http://dl.airdroid.com/AirDroid_Desktop_Client_3.6.6.2.exe'
+  checksum       = 'ef1fa33c10a2cc20544d7fc0d9d9e0a81e497bc16dff2bf1312cd2bf7265004a'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
