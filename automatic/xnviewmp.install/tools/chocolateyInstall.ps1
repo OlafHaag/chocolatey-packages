bd38c17e57cd44ac7676778de3a0fe50000409c5
Chocolatey <chocolatey@realdimensions.net>
2019-12-22 00:43:26 +0000

    AU: 3 updated - xnviewmp xnviewmp.install xnviewmp.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/49936d6e7d119bade6046688ffac1a0c37da1319

--- automatic/xnviewmp.install/tools/chocolateyInstall.ps1
+++ automatic/xnviewmp.install/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'xnviewmp.install'
   fileType       = 'exe'
-  url            = 'https://download.xnview.com/XnViewMP-win.exe'
-  url64bit       = 'https://download.xnview.com/XnViewMP-win-x64.exe'
-  checksum       = '30be7f5bebce2448e46024d953ad8697891dad34ded74f9ee262b48449192730'
+  url            = 'https://download.xnview.com/old_versions/XnViewMP-0942-win.exe'
+  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0942-win-x64.exe'
+  checksum       = '16ad4904bf2b6f2b8ab6b3b74c7164dcf791c1439b7ecd2640d0942b81e32897'
   checksumType   = 'sha256'
-  checksum64     = '574ed1f4b66ae52c45c6a144c30205813e548145437a4cf5749e79f0eebaec7f'
+  checksum64     = '20530e2e72dd2a9ae78ad70827473d37d820b90d40a2115cb36646b3322117ff'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
