3cb27fb1d3710ce1e23b08234965c1ab879b6867
Chocolatey <chocolatey@realdimensions.net>
2020-01-15 21:33:09 +0000

    AU: 1 updated - xnviewmp.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/340a46bc6bd28efb2e658d7d8b1b057645c8b2ca

--- automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
+++ automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   packageName    = 'xnviewmp.portable'
-  url            = 'https://download.xnview.com/old_versions/XnViewMP-0942-win.zip'
-  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0942-win-x64.zip'
-  checksum       = '57a235100e7e9d2d778ff2faaae8af5c2429424fea7b7e616ba7354bebc19633'
+  url            = 'https://download.xnview.com/old_versions/XnViewMP-0943-win.zip'
+  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0943-win-x64.zip'
+  checksum       = 'de5e58b6006a4f20a0362ddaa63ef859c80ccd0c30c1c1445123f2101b39c0fe'
   checksumType   = 'sha256'
-  checksum64     = '701e68260b11ad8d0b410dc8a3c679dde5a8371ed33d45d510465359ba0c5ef7'
+  checksum64     = '1f81bc39febef699db08656cc8ebce8da75034bb8fb69169e1d310218934f8b8'
   checksumType64 = 'sha256'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   validExitCodes = @(0)
