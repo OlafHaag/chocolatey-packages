eb1a6f24ea8634181e562c5fbff8263327a0c9dd
Olaf Haag <haag.olaf@gmail.com>
2020-03-23 03:24:08 +0100

    jabref.portable remove filetype search pattern from update

--- automatic/jabref.portable/update.ps1
+++ automatic/jabref.portable/update.ps1
@@ -4,7 +4,6 @@ function global:au_SearchReplace {
   @{
     "tools\chocolateyInstall.ps1" = @{
         "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
-        "(?i)(^\s*fileType\s*=\s*)('.*')"   = "`$1'$($Latest.FileType)'"
         "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64_p)'"
         "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
