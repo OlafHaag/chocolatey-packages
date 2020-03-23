46c729ee1a334121e0fc6e9565c2926d9988a019
Chocolatey <chocolatey@realdimensions.net>
2020-03-23 02:22:14 +0000

    AU: 1 updated - jabref.install [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/ed5e0a500df427b3df9ee1113f14f4873bdb26d8

--- automatic/jabref.install/tools/chocolateyInstall.ps1
+++ automatic/jabref.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName    = 'jabref'
-  fileType       = 'msi'
-  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.0-alpha/JabRef_windows-x64_5_0-alpha.exe'
-  checksum64     = 'dde2a8b76e8602b758c7dd1205a2d430187340fa61ee0cb3a952e82127dda565'
+  packageName    = 'jabref.install'
+  fileType       = 'zip'
+  url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.0/JabRef-5.0.msi'
+  checksum64     = 'c5f27955b01f57baf8ea5386c6ba81f057f21a659cdf269f23340e3313f0b8d9'
   checksumType64 = 'sha256'
   silentArgs     = '/quiet /qn /norestart'
   validExitCodes = @(0)
