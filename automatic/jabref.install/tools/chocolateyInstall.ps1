37e28c4b7ebce17578bd2d166d38a64bc2bb5217
Olaf Haag <haag.olaf@gmail.com>
2020-03-23 03:31:55 +0100

    [skip ci] set jabref file type manually

--- automatic/jabref.install/tools/chocolateyInstall.ps1
+++ automatic/jabref.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName    = 'jabref.install'
-  fileType       = 'zip'
+  fileType       = 'msi'
   url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.0/JabRef-5.0.msi'
   checksum64     = 'c5f27955b01f57baf8ea5386c6ba81f057f21a659cdf269f23340e3313f0b8d9'
   checksumType64 = 'sha256'
