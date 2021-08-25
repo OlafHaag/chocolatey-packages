61d29a43c998e26fd1c815c4d084115ea29bb6ea
Chocolatey <chocolatey@realdimensions.net>
2021-08-25 21:31:33 +0000

    AU: 3 updated - knime knime.install knime.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/41597ee53cd361ac20442ed485b63bcd07ba7c73

--- automatic/knime.install/tools/chocolateyInstall.ps1
+++ automatic/knime.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'knime.install'
   fileType       = 'exe'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.4.0%20Installer%20%2864bit%29.exe'
-  checksum64     = '37e3b07a82cdec1cb866b5c31d3f249a3042998b7062219446ac7af2985b49bd'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.4.1%20Installer%20%2864bit%29.exe'
+  checksum64     = 'a9b1399dc2aad5728a2b18ed9ec4c681b2cb90ceac0d714cf98882002ccb9a17'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- automatic/knime.portable/tools/chocolateyInstall.ps1
+++ automatic/knime.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'knime.portable'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.4.0.win32.win32.x86_64.zip'
-  checksum64     = 'b83fe030d18b9c36a95b1fd5ceba950af5679e585625af2596d0145f41dc0bf0'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.4.1.win32.win32.x86_64.zip'
+  checksum64     = '58d36a3246531066d081ec5ec5625cc0bb449e62c60dc6c42ea92b4ff8052ff2'
   checksumType64 = 'sha256'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   validExitCodes = @(0)
