2b899835e5b1407f576e3d59561188c488c3d944
Chocolatey <chocolatey@realdimensions.net>
2021-09-23 21:32:09 +0000

    AU: 3 updated - xnviewmp xnviewmp.install xnviewmp.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/c2479b4ad422415dbee67f42b0994cbb975be62d

--- automatic/xnviewmp.install/tools/chocolateyInstall.ps1
+++ automatic/xnviewmp.install/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'xnviewmp.install'
   fileType       = 'exe'
-  url            = 'https://download.xnview.com/old_versions/XnViewMP-0990-win.exe'
-  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0990-win-x64.exe'
-  checksum       = '80f488049760b99519ffa0a01142326786da05e6238ff0ebca2fc7bf0c1f09b8'
+  url            = 'https://download.xnview.com/old_versions/XnViewMP-0991-win.exe'
+  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0991-win-x64.exe'
+  checksum       = '886d7101825134bb9dbe359a7c4d21039d5597cc5118bcedafe9d1a3b60c75f6'
   checksumType   = 'sha256'
-  checksum64     = '54f471fe6094e3d73ff3fc27675b1fdda0d47cdeca1641289babcce712b78080'
+  checksum64     = '04297a56e1c9d7937a38bbfbd389f871548c27fe75098f149d844339b2bbd29f'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
+++ automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   packageName    = 'xnviewmp.portable'
-  url            = 'https://download.xnview.com/old_versions/XnViewMP-0990-win.zip'
-  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0990-win-x64.zip'
-  checksum       = 'bf0619f9cc0a91a82d428966fd2f58f1488073bd84e04479e3608b39854359ea'
+  url            = 'https://download.xnview.com/old_versions/XnViewMP-0991-win.zip'
+  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-0991-win-x64.zip'
+  checksum       = 'd71b149caca67ddf7f43e160c062931ff3cfee086c244d2a431a0ef96cb6b900'
   checksumType   = 'sha256'
-  checksum64     = '655d7bf9110c1366f574611d6692364def68fda1fa50c04125ddc3ad639b2925'
+  checksum64     = '3ff07d08833be4587778d29891486966121fa95a8c44dc8fd1c1ba4cf33cc970'
   checksumType64 = 'sha256'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   validExitCodes = @(0)
