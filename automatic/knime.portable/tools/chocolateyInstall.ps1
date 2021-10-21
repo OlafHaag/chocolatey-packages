e144de55755b520fe30e4e5f326d36b552a94f23
Chocolatey <chocolatey@realdimensions.net>
2021-10-21 21:32:20 +0000

    AU: 3 updated - knime knime.install knime.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/bc8bcf9e1ed4b1045e19a2b9bcdeac9509e182d6

--- automatic/knime.portable/tools/chocolateyInstall.ps1
+++ automatic/knime.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'knime.portable'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.4.1.win32.win32.x86_64.zip'
-  checksum64     = '58d36a3246531066d081ec5ec5625cc0bb449e62c60dc6c42ea92b4ff8052ff2'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/knime_4.4.2.win32.win32.x86_64.zip'
+  checksum64     = '8c73aa3f3eeb339be6123bece299bd78d5a907c46ca2e581ebf9a1738dcb6fd2'
   checksumType64 = 'sha256'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   validExitCodes = @(0)
