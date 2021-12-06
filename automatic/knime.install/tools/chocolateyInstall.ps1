397567269d89aa39c44f9821051976dd3a3beb26
Chocolatey <chocolatey@realdimensions.net>
2021-12-06 21:33:19 +0000

    AU: 2 updated - knime knime.install [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/f8c232e030a6a7c18dfe0180928e2aaed828baec

--- automatic/knime.install/tools/chocolateyInstall.ps1
+++ automatic/knime.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'knime.install'
   fileType       = 'exe'
-  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.4.2%20Installer%20%2864bit%29.exe'
-  checksum64     = '985474955e48480e827f17e63ad8faab942a5a49e2482568558f8888666b75fb'
+  url64bit       = 'https://download.knime.org/analytics-platform/win/KNIME%204.5.0%20Installer%20%2864bit%29.exe'
+  checksum64     = 'fa2fb50ab217b7a5d4d50d7c7e40f458d8006c052b778fe772439b8a3e3cce72'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
