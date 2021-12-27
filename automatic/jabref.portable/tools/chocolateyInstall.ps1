e7b3525b33f1e60fc68ee04e47edfc381bcee7dc
Chocolatey <chocolatey@realdimensions.net>
2021-12-27 18:11:28 +0000

    AU: 3 updated - jabref jabref.install jabref.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/d996453027f60e55852f812ad1c0a2812f8ece18

--- automatic/jabref.portable/tools/chocolateyInstall.ps1
+++ automatic/jabref.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'jabref.portable'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   url64bit       = 'https://github.com/JabRef/jabref/releases/download/v5.4/JabRef-5.4-portable_windows.zip'
-  checksum64     = '36a4d5adf47c3c3c5485e640e9de4da6fdf696a9533324b5532bc2a0b69d138f'
+  checksum64     = '5d2ff2ff5fa676ec1010f49d150c1bb34fc82f1c31e178b7db5249003cd31402'
   checksumType64 = 'sha256'
 }
 
