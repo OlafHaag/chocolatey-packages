7f090cda12f78a7227458f1f46f5f837a489d6b2
Chocolatey <chocolatey@realdimensions.net>
2022-05-05 21:31:37 +0000

    AU: 3 updated - jamovi jamovi.install jamovi.portable [skip ci] https://gist.github.com/d43292341595e09bc2463972497f7afd/e39cd58ae9f4b6b856149ffcb43067e767c93119

--- automatic/jamovi.install/tools/chocolateyInstall.ps1
+++ automatic/jamovi.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'jamovi.install'
   fileType       = 'exe'
-  url64bit       = 'https://www.jamovi.org/downloads/jamovi-2.3.6.0-win64.exe'
-  checksum64     = '11cb2598637de0eb24ee6587a59eb478706af034f76ff20a0432cacb3a33f2ae'
+  url64bit       = 'https://www.jamovi.org/downloads/jamovi-2.3.7.0-win64.exe'
+  checksum64     = 'ebee80831c420e7a87772534a3bf32fe65c097024f89cbb7927d1366ef0001f9'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/jamovi.portable/tools/chocolateyInstall.ps1
+++ automatic/jamovi.portable/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'jamovi.portable'
   UnzipLocation  = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  url64bit       = 'https://www.jamovi.org/downloads/jamovi-2.3.6.0-win64.zip'
-  checksum64     = 'a0697c14654199ffd5087b2dd800933395ff922a9c7746f03d15e33066f9bf97'
+  url64bit       = 'https://www.jamovi.org/downloads/jamovi-2.3.7.0-win64.zip'
+  checksum64     = 'ca4bcc5fae14a76dafab739c6ff62b3d1bb44afce0ce8e78fbfdef73b3f367d1'
   checksumType64 = 'sha256'
 }
 
