bd2dbfea9230baab5c11b31019ffefe89c6a0531
Erik Hanson <erik.hanson@gmail.com>
2022-10-03 13:53:40 -0500

    Repair XnViewMP Download Location
    * XnViewMP download locations have moved

--- automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
+++ automatic/xnviewmp.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = 'xnviewmp.portable'
-  url            = 'https://download.xnview.com/old_versions/XnViewMP-1.3.0-win.zip'
-  url64bit       = 'https://download.xnview.com/old_versions/XnViewMP-1.3.0-win-x64.zip'
+  url            = 'https://download.xnview.com/old_versions/XnView_MP/XnView_MP-1.03.0-win.zip'
+  url64bit       = 'https://download.xnview.com/old_versions/XnView_MP/XnView_MP-1.03.0-win-x64.zip'
   checksum       = 'fa573fd98f673d74c98c216865b7413ca8cf8c2b708248376edfa6446be07109'
   checksumType   = 'sha256'
   checksum64     = 'db56cec7e252a0d443afb98cdd019f8d8ecfd806a8945228f34e090a3a082312'
