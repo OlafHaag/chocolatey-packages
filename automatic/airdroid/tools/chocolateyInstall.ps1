1575bcb75a550256ae22c3306c5fda34d47bff86
Chocolatey <chocolatey@realdimensions.net>
2024-12-30 21:33:12 +0000

    AU: 4 updated - airdroid srwareiron srwareiron.install srwareiron.portable [skip ci] https://gist.github.com/OlafHaag/d43292341595e09bc2463972497f7afd/88a6e1017e2688d0b2f8fac2dd88260214b8bb40

--- automatic/airdroid/tools/chocolateyInstall.ps1
+++ automatic/airdroid/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'airdroid'
   fileType       = 'exe'
-  url            = 'https://dl.airdroid.com/AirDroid_Desktop_Client_3.7.2.1.exe'
-  checksum       = 'f786f0fcc11f6f690503c379b0c1423a9f6443ebf5229c5038754c17bfd51216'
+  url            = 'https://dl.airdroid.com/download/AirDroid/Win/v3.8.0.0/20241226161709/AirDroid_Desktop_Client_3.8.0.0.exe'
+  checksum       = '4b395ad2c2148ad32ee44506dc829055a4c2d8d898e062d57c47e3bb62f74844'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
--- automatic/srwareiron.install/tools/chocolateyInstall.ps1
+++ automatic/srwareiron.install/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://www.srware.net/downloads/srware_iron.exe'
   url64bit       = 'https://www.srware.net/downloads/srware_iron64.exe'
-  checksum       = 'dd9bc311500cadb93c915c0ab9ab3c634658447284393927ccfda6542ad33b3c'
+  checksum       = 'd601cec497d96a0da5f509a808a77e368f773235395b58db10687a3d0ead3c7a'
   checksumType   = 'sha256'
-  checksum64     = '695cae1170e17c8206ef84bbae47fb73159e11ccd4398bfb0f4291247c329297'
+  checksum64     = '445ce6c7ff3bb36b19077a1d291d96b7653a7449e1b7eeca749f2b01524ebcb8'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Components=Iron'
   validExitCodes = @(0)
